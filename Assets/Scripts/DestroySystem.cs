6af76f105ce8514ae1e5512d7f90133d4e63a857
nazandrey <nazandrbox@yandex.ru>
2021-07-15 22:27:45 +0300

    destroy bullets after delay

---
+++ Assets/Scripts/DestroySystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class DestroySystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var deltaTime = Time.DeltaTime;
+        
+        Entities
+            .WithName(nameof(DestroySystem))
+            .WithoutBurst()
+            .WithStructuralChanges()
+            .ForEach((Entity entity, ref DestroyData bulletData) =>
+            {
+                bulletData.DelayBeforeDestroy -= deltaTime;
+                if (bulletData.DelayBeforeDestroy <= 0)
+                    EntityManager.DestroyEntity(entity);
+            })
+            .Run();
+        
+        return inputDeps;
+    }
+}
