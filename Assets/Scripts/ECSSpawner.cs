eb1ee192bbd524c681fca7cae72442321a147a03
nazandrey <nazandrbox@yandex.ru>
2021-07-15 21:53:46 +0300

    spawn red blood cells

--- Assets/Scripts/ECSSpawner.cs
+++ Assets/Scripts/ECSSpawner.cs
@@ -9,6 +9,7 @@ public class ECSSpawner : MonoBehaviour
     public static EntityManager EntityManager;
 
     [SerializeField] private GameObject virusPrefab;
+    [SerializeField] private GameObject redBloodCellPrefab;
     [SerializeField] private int quantity;
 
     private BlobAssetStore _store;
@@ -19,19 +20,26 @@ public class ECSSpawner : MonoBehaviour
         EntityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, _store);
         var virus = GameObjectConversionUtility.ConvertGameObjectHierarchy(virusPrefab, settings);
+        var redBloodCell = GameObjectConversionUtility.ConvertGameObjectHierarchy(redBloodCellPrefab, settings);
 
+        Spawn(virus);
+        Spawn(redBloodCell);
+    }
+
+    private void Spawn(Entity virus)
+    {
         for (int i = 0; i < quantity; i++)
         {
             var instance = EntityManager.Instantiate(virus);
-            
+
             var x = Random.Range(-50, 50);
             var y = Random.Range(-50, 50);
             var z = Random.Range(-50, 50);
 
             var position = new float3(x, y, z);
             var speed = Random.Range(1f, 3f);
-            EntityManager.SetComponentData(instance, new Translation { Value = position });
-            EntityManager.SetComponentData(instance, new WanderData { Speed = speed });
+            EntityManager.SetComponentData(instance, new Translation {Value = position});
+            EntityManager.SetComponentData(instance, new WanderData {Speed = speed});
         }
     }
 
