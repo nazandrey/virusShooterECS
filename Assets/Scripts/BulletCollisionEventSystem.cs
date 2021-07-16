8563aa700c3ec5a87f8020d0b0f4dfcdc7c8a384
nazandrey <nazandrbox@yandex.ru>
2021-07-16 22:43:18 +0300

    destroy viruses through collision with bullets

---
+++ Assets/Scripts/BulletCollisionEventSystem.cs
@@ -0,0 +1,65 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using UnityEngine.PlayerLoop;
+
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
+public class BulletCollisionEventSystem : JobComponentSystem
+{
+    private BuildPhysicsWorld _buildPhysicsWorld;
+    private StepPhysicsWorld _stepPhysicsWorld;
+    
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        _buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        _stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    }
+
+    private struct CollisionEventImpulseJob : ICollisionEventsJob
+    {
+        [ReadOnly] public ComponentDataFromEntity<BulletData> BulletGroup;
+        public ComponentDataFromEntity<VirusData> VirusGroup;
+        
+        public void Execute(CollisionEvent collisionEvent)
+        {
+            var entitiyA = collisionEvent.Entities.EntityA;
+            var entitiyB = collisionEvent.Entities.EntityB;
+
+            var isTargetA = VirusGroup.Exists(entitiyA);
+            var isTargetB = VirusGroup.Exists(entitiyB);
+            
+            var isBulletA = BulletGroup.Exists(entitiyA);
+            var isBulletB = BulletGroup.Exists(entitiyB);
+
+            if (isTargetA && isBulletB)
+                MarkVirusNotAlive(entitiyA);
+            
+            if (isTargetB && isBulletA)
+                MarkVirusNotAlive(entitiyB);
+        }
+
+        private void MarkVirusNotAlive(Entity virusEntity)
+        {
+            var aliveComponentData = VirusGroup[virusEntity];
+            aliveComponentData.IsAlive = false;
+            VirusGroup[virusEntity] = aliveComponentData;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = new CollisionEventImpulseJob
+        {
+            BulletGroup = GetComponentDataFromEntity<BulletData>(),
+            VirusGroup = GetComponentDataFromEntity<VirusData>(),
+        }.Schedule(_stepPhysicsWorld.Simulation, ref _buildPhysicsWorld.PhysicsWorld, inputDeps);
+        
+        jobHandle.Complete();
+        
+        return jobHandle;
+    }
+    
+}
---
+++ Assets/Scripts/VirusSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+[UpdateAfter(typeof(BulletCollisionEventSystem))]
+public class VirusSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities
+            .WithName(nameof(VirusSystem))
+            .WithoutBurst()
+            .WithStructuralChanges()
+            .ForEach((ref VirusData virusData, in Entity entity) =>
+            {
+                if (!virusData.IsAlive)
+                {
+                    EntityManager.DestroyEntity(entity);
+                }
+            }).Run();
+
+        return inputDeps;
+    }
+}
